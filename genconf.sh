1b4bd54a566829ae90e0b9d4895cb60ed4b87e43
npmaile <npmaile@gmail.com>
2018-11-23 01:22:13 -0600

    made a config generator

--- genconf.sh
+++ genconf.sh
@@ -1,23 +1,25 @@
+#!/usr/bin/bash
+echo -e "
 events{
 	worker_connections 4096;
 }
 http {
 	server{
 		location / {
-			root /data;
+			root "$(pwd)"/data;
 			index index.php;
 			disable_symlinks off;
 			}
-		location ~ [^/]\.php(/|$) {
-			fastcgi_split_path_info ^(.+?\.php)(/.*)$;
-			if (!-f $document_root$fastcgi_script_name){
+		location ~ [^/]\\.php(/|$) {
+			fastcgi_split_path_info ^(.+?\\.php)(/.*)$;
+			if (!-f \$document_root\$fastcgi_script_name){
 				return 404;
 				}
-			root /data;	
-			fastcgi_param HTTP_PROXY "";
+				root "$(pwd)"/data;	
+			fastcgi_param HTTP_PROXY \"\";
 			fastcgi_pass unix:/var/run/php-fpm/php-fpm.sock;
 			fastcgi_index index.php;
 			include /etc/nginx/fastcgi_params;
 			}
 	}
-}
+}"
