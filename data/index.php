487ee74e3bc0ada6873fa3a1cdacf075c6725da3
root <root@storage.my.domain>
2020-08-22 23:49:52 -0500

    remove seaparate photos script.

--- data/index.php
+++ data/index.php
@@ -1,12 +1,66 @@
 <html>
 	<head>
 <link rel="stylesheet" href="style.css">
+<script src="script.js"/></script>
+<script>
+var medialist=[
+<?php
+
+$mediaPatterns;
+
+if (!empty($_GET['media'])){
+	$mediaPatterns = $_GET['media'];
+}
+
+
+$rootdir = $_SERVER['DOCUMENT_ROOT'];
+$workingdir = $rootdir;
+if (!empty($_GET['path'])){
+	$requestpath = urldecode($_GET['path']);
+	$workingdir = ("$rootdir/$requestpath");
+}
+
+function fileextension($name){
+return substr($name, strrpos($name, '.')+1, strlen($name)-strrpos('.', $name));
+}
+
+
+function getfiles($rootpath,$patterns){
+	$ret = array();
+	foreach (scandir($rootpath) as $file){
+		$pathToCheck = "$rootpath/$file";	
+		if(substr($pathToCheck, -2) == "/."){
+		}
+		elseif(substr($pathToCheck, -3) == "/.."){
+		}
+		elseif(is_dir("$pathToCheck")){
+			$ret = array_merge($ret,getfiles("$pathToCheck",$patterns));
+		}
+		elseif(in_array(fileextension("$pathToCheck"),$patterns)){
+			array_push($ret,"$pathToCheck");
+		}
+	}
+	return $ret;
+}
+
+$filesToDisplay = getfiles($workingdir, $mediaPatterns);
+shuffle($filesToDisplay);	
+foreach ($filesToDisplay as $file){
+	$mediaLink = str_replace("$rootdir/",'',$file);	
+	echo("\"$mediaLink\",\n");
+}
+
+
+
+?>
+]
+	</script>
 	</head>	
 	<body>
 		<?php
 
 			function PhotoLink($link){
-				echo("<a href=/photos.php?path=$link&media[]=jpg&media[]=jpeg&media[]=png&media[]=gif> photos</a>");
+				echo("<a href=/index.php?path=$link&media[]=jpg&media[]=jpeg&media[]=png&media[]=gif> photos</a>");
 			}
 
 			function IndexLink($link,$path){
@@ -15,7 +69,7 @@
 			}
 
 			function VidLink($link){
-				echo("<a href=/photos.php?path=$link&media[]=mp4&media[]=wmv&media[]=webm> videos</a>");
+				echo("<a href=/index.php?path=$link&media[]=mp4&media[]=wmv&media[]=webm> videos</a>");
 			}
 
 
@@ -46,5 +100,9 @@
 			}
 			echo("</table>");
 		?>
+		<p id="gallery">
+					
+		<p>
+
 	</body>
 </html>
