213b487b5057d9f7e221aa28aeb146d4cd901b4a
Nate Maile <npmaile@gmail.com>
2023-09-04 00:27:13 -0400

    add randomize button to frontend

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ var iter = 0
 var medialist = []
 var medialist
 
-setMediaHeight = function(){
+setMediaHeight = function() {
 	var height = window.innerHeight;
 	var medias = document.getElementsByClassName('image video-js');
 	for (var i = 0; i < medias.length; i++) {
@@ -10,79 +10,79 @@ setMediaHeight = function(){
 	}
 }
 
-addimage = function(image,id){
+addimage = function(image, id) {
 	var paragraph = document.getElementById("gallery");
 	var newimage = document.createElement("img");
-	newimage.setAttribute('src',"data" + image);
+	newimage.setAttribute('src', "data" + image);
 	newimage.setAttribute('id', id);
-	newimage.setAttribute('class','image');
+	newimage.setAttribute('class', 'image');
 	paragraph.appendChild(newimage);
 }
 
-addvideo = function(video,id){
+addvideo = function(video, id) {
 	var idString = "video" + String(id)
 	var paragraph = document.getElementById("gallery");
 	var newvid = document.createElement("video");
 	var source = document.createElement("source");
-	source.setAttribute('src',"data" + video);
+	source.setAttribute('src', "data" + video);
 	newvid.appendChild(source);
-	newvid.setAttribute('id',idString);
-	newvid.setAttribute('class','video-js');
-	newvid.setAttribute('data-setup','{"controls":"true", "preload": false}');
-	newvid.setAttribute('loop',"true");
-	newvid.setAttribute('responsive',"true");
-	newvid.setAttribute('fill',"true");
+	newvid.setAttribute('id', idString);
+	newvid.setAttribute('class', 'video-js');
+	newvid.setAttribute('data-setup', '{"controls":"true", "preload": false}');
+	newvid.setAttribute('loop', "true");
+	newvid.setAttribute('responsive', "true");
+	newvid.setAttribute('fill', "true");
 	paragraph.appendChild(newvid);
 
 	videojs(idString);
 
 	var title = document.createElement("div");
 	title.innerHTML = video;
-	title.setAttribute('class','title');
+	title.setAttribute('class', 'title');
 	paragraph.appendChild(title);
 }
 
-genMedia = function(num){
-	while(num > 0 && iter < medialist.length){
-		if (/.*\.(jpeg|jpg|gif|png)$/.test(medialist[iter].toLowerCase())){
+genMedia = function(num) {
+	while (num > 0 && iter < medialist.length) {
+		if (/.*\.(jpeg|jpg|gif|png)$/.test(medialist[iter].toLowerCase())) {
 			addimage(medialist[iter]);
 		}
-		else if (/.*\.(m4v|mp4|mov|webm)$/.test(medialist[iter].toLowerCase())){
-			addvideo(medialist[iter],iter);
+		else if (/.*\.(m4v|mp4|mov|webm)$/.test(medialist[iter].toLowerCase())) {
+			addvideo(medialist[iter], iter);
 		}
-		iter ++;
-		num --;
+		iter++;
+		num--;
 	}
 }
 
-loadmoaronbottom = function(){
+loadmoaronbottom = function() {
 	var buffer = 20
 	var progress, totalheight;
 	totalheight = document.body.scrollHeight;
 	progress = window.scrollY + window.innerHeight;
-	if(totalheight <= progress + buffer){
+	if (totalheight <= progress + buffer) {
 		genMedia(buffer);
 	}
 }
 
-function spaLink(element, apilink){
+function spaLink(element, apilink) {
 	element.addEventListener('click', function(event) {
 		event.preventDefault();
 		reloadsite(apilink)
 	})
 }
 
-function addDirsToList(listing, dirs){
+function addDirsToList(listing, dirs) {
 	list = document.createElement("ul")
 	listing.appendChild(list)
-	for (let i = 1; i < dirs.length; i++){
+	for (let i = 1; i < dirs.length; i++) {
 		li = document.createElement("li")
 		a = document.createElement("a")
 		a.innerHTML = dirs[i]
 		a.href = "api" + dirs[i]
 		a.addEventListener('click', function(event) {
 			event.preventDefault();
-			reloadsite("api"+dirs[i]+ "?dir_depth=1")
+			reloadsite("api" + dirs[i] + "?dir_depth=1")
 		})
 		li.appendChild(a)
 
@@ -91,7 +91,7 @@ function addDirsToList(listing, dirs){
 		a.href = "api" + dirs[i]
 		a.addEventListener('click', function(event) {
 			event.preventDefault();
-			reloadsite("api"+dirs[i]+ "?media_extensions=jpg,png,jpeg,gif&dir_depth=1")
+			reloadsite("api" + dirs[i] + "?media_extensions=jpg,png,jpeg,gif&dir_depth=1")
 		})
 		li.appendChild(a)
 
@@ -100,7 +100,7 @@ function addDirsToList(listing, dirs){
 		a.href = "api" + dirs[i]
 		a.addEventListener('click', function(event) {
 			event.preventDefault();
-			reloadsite("api"+dirs[i]+ "?media_extensions=m4v,mp4,mov,webm&dir_depth=1")
+			reloadsite("api" + dirs[i] + "?media_extensions=m4v,mp4,mov,webm&dir_depth=1")
 		})
 		li.appendChild(a)
 
@@ -108,29 +108,30 @@ function addDirsToList(listing, dirs){
 	}
 }
 
-async function generateSite(url){
+async function generateSite(url) {
 	res = await fetch(url);
 	txt = await res.text()
 	data = JSON.parse(txt)
 	head = document.getElementById("current_dir")
 	splitParts = data.current_dir.split("/")
-	for (let i=0; i < splitParts.length; i++){
+	for (let i = 0; i < splitParts.length; i++) {
 		a = document.createElement("a")
-		if (i==0) {
+		if (i == 0) {
 			a.innerHTML = "root"
 			a.href = "api/"
-			spaLink(a,"api/?dir_depth=1")
+			spaLink(a, "api/?dir_depth=1")
 			head.appendChild(a)
 			continue
 		}
 		a.innerHTML = "/" + splitParts[i]
-		a.href = "api" + splitParts.slice(0,i+1).join("/")
+		a.href = "api" + splitParts.slice(0, i + 1).join("/")
 		a.addEventListener('click', function(event) {
 			event.preventDefault();
-			reloadsite("api"+splitParts.slice(0,i+1).join("/") + "?dir_depth=1")
+			reloadsite("api" + splitParts.slice(0, i + 1).join("/") + "?dir_depth=1")
 		})
 		head.appendChild(a)
 	}
+
 	plink = document.createElement("button")
 	plink.innerHTML = "    photos"
 	spaLink(plink, "api" + data.current_dir + "?media_extensions=jpg,png,jpeg,gif&dir_depth=1")
@@ -148,19 +149,25 @@ async function generateSite(url){
 	genMedia(20);
 }
 
-async function reloadsite(url){
-	toclear = ["current_dir","dirs","gallery"]
-	for (let i = 0; i < toclear.length; i++){
+async function reloadsite(url) {
+	toclear = ["current_dir", "dirs", "gallery"]
+	for (let i = 0; i < toclear.length; i++) {
 		x = document.getElementById(toclear[i])
-		for (let j = 0; j < x.children; x++){
+		for (let j = 0; j < x.children; x++) {
 			c.removeChild(x.children[0])
 		}
 		x.innerHTML = ""
 	}
 	medialist = []
 	alreadyloadedonce = 0
-	await generateSite(url)	
-	if (alreadyloadedonce == 0){
+	if (document.getElementById('randomizeCheckbox').checked) {
+		beginning = new URL(window.location.origin)
+		const urlWithParams = new URL(url, beginning);
+		urlWithParams.searchParams.append("file_sort", "random");
+		url = urlWithParams.href
+	}
+	await generateSite(url)
+	if (alreadyloadedonce == 0) {
 		genMedia(20);
 		alreadyloadedonce = 1;
 	}
@@ -168,27 +175,27 @@ async function reloadsite(url){
 
 let keepautoscrolling = false
 
-function autoscroll(){
-	window.scrollBy(0,1);
+function autoscroll() {
+	window.scrollBy(0, 1);
 	if (!keepautoscrolling) {
 		return
 	}
 	scrolldelay = setTimeout(autoscroll, 10);
 }
 
-function setupAutoScrolling(){
+function setupAutoScrolling() {
 	let sb = document.getElementById("scrollbutton")
-	sb.addEventListener('click', function(event) {
+	sb.addEventListener('click', function(_event) {
 		keepautoscrolling = true
 		autoscroll()
 	})
 	let hw = document.getElementById("stopscrollbutton")
-	hw.addEventListener('click', function(event) {
+	hw.addEventListener('click', function(_event) {
 		keepautoscrolling = false
 	})
 	let top = document.getElementById("returnToTop")
-	top.addEventListener('click', function(event) {
-		window.scrollTo(0,0)
+	top.addEventListener('click', function(_event) {
+		window.scrollTo(0, 0)
 	})
 }
 
@@ -196,7 +203,7 @@ window.onresize = setMediaHeight;
 window.onscroll = loadmoaronbottom;
 var alreadyloadedonce = 0
 
-window.onload = function(){
+window.onload = function() {
 	reloadsite("api/?dir_depth=1")
 	setupAutoScrolling()
 }
